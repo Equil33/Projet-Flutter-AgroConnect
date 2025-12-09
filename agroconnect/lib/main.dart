725900d311f77cd754eba5d25280956cbb221cbe
Faiz DANDJA <dandjaequil33@gmail.com>
2025-12-09 10:06:51 +0000

    Ajout de main.dart

---
+++ agroconnect/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'accueil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AgroConnect',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+
+      ),
+      home: const Accueil(),
+    );
+  }
+}
